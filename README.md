c6036e058b25075158172120ee015e9086cf68bf
Dominik Moritz <domoritz@gmail.com>
2017-01-14 23:56:43 -0800

    Initial import of all past schemas.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # schema
+
 JSON schema for Vega and Vega-Lite
+
+## Url format
+
+```
+https://vega.github.io/schema/[library]/[version].json
+```
+
+* `[library]` can be `vega` or `vega-lite`
+* `[version]` defines the version of the library for which you get the schema. Underspecified versions match the latest version of the unspecified part. For example v1 matches the latest major release, v1.1 matches the latest minor release, and v1.1.1 matches an exact version.
---
+++ download.py
@@ -0,0 +1,30 @@
+import json
+import urllib.error, urllib.request
+import os
+import errno
+
+def force_symlink(file1, file2):
+    try:
+        os.symlink(file1, file2)
+    except OSError as e:
+        if e.errno == errno.EEXIST:
+            os.remove(file2)
+            os.symlink(file1, file2)
+
+for lib in ['vega', 'vega-lite']:
+    with open('{lib}.json'.format(lib=lib)) as f:
+        parsed = json.load(f)
+        for tag in parsed:
+            version = tag['ref'].replace('refs/tags/', '')
+            url = 'https://raw.githubusercontent.com/vega/{lib}/{version}/{lib}-schema.json'.format(version=version, lib=lib)
+            try:
+                schema = "{lib}/{version}.json".format(version=version, lib=lib)
+                urllib.request.urlretrieve(url, schema)
+                split = version.split('.')
+                force_symlink(schema, "{lib}/{version}.json".format(version=split[0], lib=lib))
+                force_symlink(schema, "{lib}/{version}.json".format(version=split[0]+'.'+split[1], lib=lib))
+            except urllib.error.HTTPError as e:
+                if e.code == 404:
+                    pass
+                else:
+                    raise
