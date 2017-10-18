67b54e28c479ff07647b1044628f184de6d745e6
Dominik Moritz <domoritz@gmail.com>
2017-10-17 22:01:37 -0700

    Add space

--- index.ts
+++ index.ts
@@ -3,6 +3,6 @@
  */
 export default function(url: string) {
     const regex = /\/schema\/([\w-]+)\/([\w\.\-]+)\.json$/g;
-    const [library, version] = regex.exec(url).slice(1,3);
+    const [library, version] = regex.exec(url).slice(1, 3);
     return {library, version};
 }
