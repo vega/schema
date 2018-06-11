ab67f4fa34fd0e9a506339058cb853a7ed9e96e6
Dominik Moritz <domoritz@gmail.com>
2018-06-11 16:51:04 -0700

    Stricter types

--- index.ts
+++ index.ts
@@ -4,5 +4,5 @@
 export default function(url: string) {
     const regex = /\/schema\/([\w-]+)\/([\w\.\-]+)\.json$/g;
     const [library, version] = regex.exec(url)!.slice(1, 3);
-    return {library, version};
+    return {library: library as 'vega' | 'vega-lite', version};
 }
