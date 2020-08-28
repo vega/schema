22ff8d1405a8b8d67e232b683cec268633ed8b83
Dominik Moritz <domoritz@gmail.com>
2020-08-27 21:19:39 -0700

    chore: use microbundle, make tests work

---
+++ test.ts
@@ -0,0 +1,19 @@
+import parser from '.';
+
+test('parses vega url correctly', function () {
+    const parsed = parser('https://vega.github.io/schema/vega/v2.5.2.json');
+    expect(parsed.library).toBe('vega');
+    expect(parsed.version).toBe('v2.5.2');
+});
+
+test('parses vega-lite url correctly', function () {
+    const parsed = parser('https://vega.github.io/schema/vega-lite/v1.3.1.json');
+    expect(parsed.library).toBe('vega-lite');
+    expect(parsed.version).toBe('v1.3.1');
+});
+
+test('parses url with alpha correctly', function () {
+    const parsed = parser('https://vega.github.io/schema/vega-lite/v2.0.0-alpha.0.json');
+    expect(parsed.library).toBe('vega-lite');
+    expect(parsed.version).toBe('v2.0.0-alpha.0');
+});
